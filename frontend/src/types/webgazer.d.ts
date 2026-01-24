f949c209c25e549c72b42f1724f32833ff2b81f5
Callum Mackenzie <alexxander1611@gmail.com>
2026-01-24 15:25:27 -0800

    Format

--- frontend/src/types/webgazer.d.ts
+++ frontend/src/types/webgazer.d.ts
@@ -1,28 +1,28 @@
 declare module "webgazer" {
-  type GazeData = { x: number; y: number } | null;
+	type GazeData = { x: number; y: number } | null;
 
-  type WebGazer = {
-    setRegression: (name: string) => WebGazer;
-    setGazeListener: (
-      callback: (data: GazeData, elapsedTime?: number) => void
-    ) => WebGazer;
-    saveDataAcrossSessions: (value: boolean) => WebGazer;
-    begin: () => Promise<void> | void;
-    end: () => void;
-    clearData: () => void;
-    showVideoPreview: (show: boolean) => WebGazer;
-    showPredictionPoints: (show: boolean) => WebGazer;
-    applyKalmanFilter: (enabled: boolean) => WebGazer;
-    recordScreenPosition: (x: number, y: number, eventType?: string) => void;
-    getStoredPoints: () => [number[], number[]];
-    addMouseEventListeners: () => void;
-    removeMouseEventListeners: () => void;
-    params: {
-      storingPoints: boolean;
-      applyKalmanFilter: boolean;
-    };
-  };
+	type WebGazer = {
+		setRegression: (name: string) => WebGazer;
+		setGazeListener: (
+			callback: (data: GazeData, elapsedTime?: number) => void
+		) => WebGazer;
+		saveDataAcrossSessions: (value: boolean) => WebGazer;
+		begin: () => Promise<void> | void;
+		end: () => void;
+		clearData: () => void;
+		showVideoPreview: (show: boolean) => WebGazer;
+		showPredictionPoints: (show: boolean) => WebGazer;
+		applyKalmanFilter: (enabled: boolean) => WebGazer;
+		recordScreenPosition: (x: number, y: number, eventType?: string) => void;
+		getStoredPoints: () => [number[], number[]];
+		addMouseEventListeners: () => void;
+		removeMouseEventListeners: () => void;
+		params: {
+			storingPoints: boolean;
+			applyKalmanFilter: boolean;
+		};
+	};
 
-  const webgazer: WebGazer;
-  export default webgazer;
+	const webgazer: WebGazer;
+	export default webgazer;
 }
